5c0e9be3a46038834a09bd3961d700a73fc30012
adi.lewenstein <adi.lewenstein@sisense.com>
2019-06-04 18:57:41 +0300

    Arrow function refactoring, List notes command

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,6 +2,9 @@ const notes = require("./notes.js");
 const chalk = require("chalk");
 const yargs = require("yargs");
 
+
+yargs.version = '1.1.0'
+
 yargs.command({
     command: "add",
     describe: "Add a new Note",
@@ -17,7 +20,7 @@ yargs.command({
             type: "string"
         }
     },
-    handler: function (argv) {
+    handler (argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -26,20 +29,41 @@ yargs.command({
     command: "remove",
     describe: "Remove a note",
     builder: {
-        title:{
-            describe:"The title of the note to remove",
+        title: {
+            describe: "The title of the note to remove",
             demandOption: true,
             type: "string"
         }
     },
-    handler: function(argv){
-        notes.removeNote(argv.title)
+    handler (argv) { 
+        notes.removeNote(argv.title) 
     }
 })
 
 yargs.command({
+    command: "list",
+    describe: "List all notes",
+    handler (argv){
+        notes.listNotes()
+    }
+})
+
+yargs.command({
+    command: "read",
+    describe: "Read a note",
+    builder: {
+        title:{
+            describe: "The title of the note you want to read",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler(argv){}
+})
+
+yargs.command({
     command: 'test',
-    handler:function(){
+    handler () {
         notes.addNote('Rule 1', "Don't pick up the phone, you know he's only calling cause he's drunk and alone")
         notes.addNote("Rule 2", "Don't let him in, you'll have to kick him out again")
         notes.removeNote("Rule 3")
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -3,15 +3,13 @@ const chalk = require('chalk');
 
 const filePath = './notes.json';
 
-function getNotes() {
-    console.log('Your notes')
-};
+const getNotes = () => {
+    return 'Your notes'
+}
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function(note){
-        return note.title === title;
-    })
+    const duplicateNotes = notes.filter((note)=> note.title === title);
 
     if (duplicateNotes.length === 0){
         notes.push({
@@ -27,11 +25,9 @@ const addNote = function (title, body) {
     
 }
 
-const removeNote = function(title){
+const removeNote = (title) => {
     const notes = loadNotes();
-    const newNotes = notes.filter(function(note){
-        return note.title !== title
-    })
+    const newNotes = notes.filter((note)=> note.title !== title)
 
     if (notes.length > newNotes.length){
         saveNotes(newNotes)
@@ -44,8 +40,15 @@ const removeNote = function(title){
     
 }
 
+const listNotes = () => {
+    const notes = loadNotes();
+    console.log(chalk.cyan.underline('Your notes:'))
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
         const notesBuffer = fs.readFileSync(filePath);
         const notesJSON = notesBuffer.toString();
@@ -56,13 +59,13 @@ const loadNotes = function () {
     }
 
 }
-const saveNotes = function(notes){
+const saveNotes = (notes) => {
     const notesJSON = JSON.stringify(notes);
     fs.writeFileSync(filePath, notesJSON);
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes
 };
