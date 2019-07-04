e49882bd9e00abc67855ba9c0d3e25731d65b122
AdiLew <adilu.lev@gmail.com>
2019-07-05 00:46:20 +0300

    404 and footer

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -49,6 +49,19 @@ app.get('/weather',(req,res)=>{
     res.send({location: 'Tel Aviv', forecast:'It\'s gonna be hot and awesome'})
 })
 
+app.get('/help/*',(req, res)=>{
+    res.render('404', {
+        title: 'Oh no!',
+        error: 'Support article not found'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title: 'Oh no!',
+        error:'Page not found :('
+    })
+})
 
 app.listen(3000, ()=>{
     console.log('The server is up on port 3000')
