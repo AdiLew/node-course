e3bdbadfe9a33c64c5a0fa29983c445f4c84ab48
adi.lewenstein <adi.lewenstein@sisense.com>
2019-07-17 11:45:21 +0300

    Use PORT environment variable

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast.js')
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -55,25 +56,25 @@ app.get('/weather', (req, res) => {
             error: 'An address must be provided!'
         })
     }
-    
-    geocode( address, (error, {lat, lon, placeName}={}) => {
+
+    geocode(address, (error, { lat, lon, placeName } = {}) => {
         if (error) {
-            return res.send({error})
+            return res.send({ error })
         }
-    
+
         forecast(lat, lon, (error, forecast) => {
             if (error) {
-                return res.send({error})
+                return res.send({ error })
             }
-    
+
             res.send({
                 address,
                 location: placeName,
                 forecast
             })
-        }) 
+        })
     })
-    
+
     // res.send({ 
     //     address: address, forecast: 'It\'s gonna be hot and awesome' })
 })
@@ -102,6 +103,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('The server is up on port 3000')
+app.listen(port, () => {
+    console.log(`The server is up on port ${port}`);
 })
